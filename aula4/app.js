0a757741dd79c60f1cae667889e3139e7b332048
Wanderson <Wanderson.com>
2025-07-01 19:53:33 -0300

    #001 - Commit  Inicial

---
+++ aula4/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const postRoutes = require('./routes/postRoutes');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', postRoutes);
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
---
+++ aula4/controllers/postController.js
@@ -0,0 +1,21 @@
+const postModel = require('../models/postModel');
+const fs = require('fs');
+const path = require('path');
+
+exports.listarPosts = async (req, res) => {
+  const posts = await postModel.buscarPosts();
+
+  fs.readFile(path.join(__dirname, '../views/index.html'), 'utf8', (err, html) => {
+    if (err) return res.status(500).send('Erro ao carregar a view');
+
+    const conteudo = posts.slice(0, 10).map(post => `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `).join('');
+
+    const pagina = html.replace('{{conteudo}}', conteudo);
+    res.send(pagina);
+  });
+};
